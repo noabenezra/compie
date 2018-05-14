0e2cc0e26087fdeecba58bff8737ddb904eff33d
noa <noabenezra@gmail.com>
2018-05-14 17:20:19 +0300

    3

--- website/FlickrPic.js
+++ website/FlickrPic.js
@@ -4,32 +4,16 @@
 angular.module('myApp', ['ngMaterial'])
 
   .controller('AppCtrl', function ($scope, $mdDialog) {
-    debugger;
     $scope.status = '  ';
     $scope.customFullscreen = false;
-    $scope.apiFlicker = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&user_id=141840526@N02&format=json&nojsoncallback=1&per_page=500";
-
-    /*$(document).ready(function () {
-      debugger;
-      $("md-button").click(function () {
-        debugger;
-        $.getJSON(apiFlicker, function (result) {
-          debugger;
-          $.each(result, function (i, photo) {
-            var photoList = photo.photo;
-          });
-        });
-      });
-    });*/
-
     $scope.showAdvanced = function (ev) {
       $mdDialog.show({
         controller: DialogController,
         templateUrl: 'Dialog.html',
         parent: angular.element(document.body),
         targetEvent: ev,
-        clickOutsideToClose: true,
-        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+        clickOutsideToClose: false,
+        fullscreen: $scope.customFullscreen
       })
         .then(function (answer) {
           $scope.status = 'You said the information was "' + answer + '".';
@@ -39,16 +23,13 @@ angular.module('myApp', ['ngMaterial'])
     };
 
     function DialogController($scope, $mdDialog) {
-
-      $.getJSON(apiFlicker, function (result) {
-        debugger;
-        $.each(result, function (i, photo) {
-          debugger;
-          $scope.photoList = photo.photo;
+      $scope.apiFlicker = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&user_id=141840526@N02&format=json&nojsoncallback=1&per_page=500";
+      $.getJSON($scope.apiFlicker, function (result) {
+        $scope.photoList = result.photos.photo;
+        $scope.photoList.forEach(function (element) {
+          element.url = "https://farm1.staticflickr.com/" + element.server + "/" + element.id + "_" + element.secret + "_m.jpg";
         });
       });
-
-
 
       $scope.hide = function () {
         $mdDialog.hide();
