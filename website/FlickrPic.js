48d2d285b5ab5bc7a3750dd118595538663a43e4
noa <noabenezra@gmail.com>
2018-05-14 17:54:37 +0300

    5

--- website/FlickrPic.js
+++ website/FlickrPic.js
@@ -20,20 +20,25 @@ angular.module('myApp', ['ngMaterial'])
     };
     function DialogController($scope, $mdDialog) {
       $scope.apiFlicker = "https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&user_id=141840526@N02&format=json&nojsoncallback=1&per_page=500";
+
       $.getJSON($scope.apiFlicker, function (result) {
         $scope.photoList = result.photos.photo;
         $scope.photoList.forEach(function (element) {
           element.url = "https://farm1.staticflickr.com/" + element.server + "/" + element.id + "_" + element.secret + "_m.jpg";
         });
       });
+
       $scope.hide = function () {
         $mdDialog.hide();
       };
+
       $scope.cancel = function () {
         $mdDialog.cancel();
       };
+
       $scope.answer = function (answer) {
         $mdDialog.hide(answer);
       };
+      
     }
   });
